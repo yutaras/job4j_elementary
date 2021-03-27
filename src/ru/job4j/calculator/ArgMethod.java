b5f40b604567cd41a7c4cc60acca5ad16fbb738b
Yuriy Tarasenko <maxjeff@mail.ru>
2021-03-27 15:52:23 +0300

    4.1. Аргументы [#505185]

---
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -0,0 +1,11 @@
+package ru.job4j.calculator;
+
+public class ArgMethod {
+    public static void hello(String name) {
+        System.out.println("Hello, " + name);
+    }
+    public static void main(String[] args) {
+        String name = "Petr Arsentev";
+        ArgMethod.hello(name);
+    }
+}
