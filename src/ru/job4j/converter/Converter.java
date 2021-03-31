c5ca37af621e6ae9fc800b02f83016c16b2bb055
Yuriy Tarasenko <maxjeff@mail.ru>
2021-03-31 21:25:20 +0300

    1.2. Входные данные в тесте [#505212]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -11,15 +11,9 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        int in1 = 140;
-        int expected1 = 2;
-        int out1 = Converter.rubleToEuro(in1);
-        boolean passed1 = expected1 == out1;
-        System.out.println("140 rubles are 2 euro. Test result : " + passed1);
-        int in2 = 120;
-        int expected2 = 2;
-        int out2 = Converter.rubleToDollar(in2);
-        boolean passed2 = expected2 == out2;
-        System.out.println("120 rubles are 2 dollars. Test result : " + passed2);
+        int euro = Converter.rubleToEuro(140);
+        int dollar = Converter.rubleToDollar(180);
+        System.out.println("140 rubles are " + euro + " euro.");
+        System.out.println("180 rubles are " + dollar + " dollar.");
     }
 }
