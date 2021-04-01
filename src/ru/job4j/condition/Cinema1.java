6a2b9d46d7ed846cae49f577dcdba4a05d8954d7
Yuriy Tarasenko <maxjeff@mail.ru>
2021-04-01 20:46:34 +0300

    4.3.1. Оператор && [#505127]

---
+++ src/ru/job4j/condition/Cinema1.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class Cinema1 {
+    public static void permission(boolean allowByParent, boolean hasMoney) {
+        if (allowByParent && hasMoney) {
+            System.out.println("I can go to the cinema.");
+        } else {
+            System.out.println("I can't.");
+        }
+    }
+
+    public static void main(String[] args) {
+        permission(true, true);
+        permission(true, false);
+        permission(false, true);
+        permission(false, false);
+    }
+}
