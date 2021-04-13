21c3d25f11b4f1a61d186f35a9edf0268a21cd75
Yuriy Tarasenko <maxjeff@mail.ru>
2021-04-13 15:13:29 +0300

    6.5.1. Поиск минимального числа в массиве. [#505150]

--- src/ru/job4j/array/Min.java
+++ src/ru/job4j/array/Min.java
@@ -3,9 +3,9 @@ package ru.job4j.array;
 public class Min {
     public static int findMin(int[] array) {
         int min = array[0];
-        for (int i : array) {
-            if (i < min) {
-                min = i;
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] < min) {
+                min = array[index];
             }
         }
         return min;
